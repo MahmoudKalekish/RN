b7623cdb26121f45f7e7e3d839e6d145effad5c6
Mahmoud Kalekish <mahmoudkalekish18@gmail.com>
2023-03-14 19:50:21 +0200

    LOGIN

--- App.js
+++ App.js
@@ -1,87 +1,163 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Dashboard } from './scenes/dashboard/index';
-import { useNavigation } from '@react-navigation/native';
+import React, { useState, useEffect } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const Stack = createNativeStackNavigator();
-
-// async function loginUser(credentials) {
-//   return fetch('http://localhost:8000/api/login', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(credentials)
-//   })
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
-//     }
-//     return response.json();
-//   })
-//   .then(data => {
-//     // In React Native, use AsyncStorage to save data locally
-//     // See https://reactnative.dev/docs/asyncstorage
-//     return data;
-//   })
-//   .catch(error => {
-//     console.error('Error:', error);
-//     throw error;
-//   });
-// }
-
-// const navigation = useNavigation();
-
-
-
-
-
-export default function App() {
-  const [username, setUserName] = useState('');
+const LoginPage = ({ onLogin }) => {
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState('');
 
-  const handleSubmit = async () => {
+  const handleLogin = async () => {
     try {
-      const response = await loginUser({
-        username,
-        password
+      const response = await fetch('http://192.168.0.100:8000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+          username: email,
+          password: password,
+        }),
       });
-      // Save token and is_super value in app state or AsyncStorage
-      console.log(response.token, response.is_super);
-      NavigationContainer.navigate('Dashboard'); // Navigate to Dashboard after successful login
+
+      if (!response.ok) {
+        throw new Error('Invalid email or password.');
+      }
+
+      // Check if the response contains a token or other authentication data
+      const data = await response.json();
+      const token = data.token;
+
+      if (token) {
+        // Store the token in AsyncStorage
+        await AsyncStorage.setItem('token', token);
+        onLogin();
+      } else {
+        setError('An error occurred during login.');
+      }
     } catch (error) {
-      console.error('Error:', error);
-      alert('Error: Invalid Username or Password');
+      console.error(error);
+      setError(error.message);
     }
-  }
+  };
 
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login">
-          {() => (
-            <View>
-              <Text>Username</Text>
-              <TextInput
-                value={username}
-                onChangeText={setUserName}
-              />
-              <Text>Password</Text>
-              <TextInput
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry={!showPassword}
-              />
-              <Button title="Submit" onPress={handleSubmit} />
-            </View>
-          )}
-        </Stack.Screen>
-        {/* Define Dashboard component */}
-        <Stack.Screen name="Dashboard" component={Dashboard} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry={true}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
+      <Text style={styles.error}>{error}</Text>
+    </View>
   );
-}
+};
+
+const HomePage = ({ onLogout }) => {
+  const handleLogout = async () => {
+    // Remove the token from AsyncStorage
+    await AsyncStorage.removeItem('token');
+    // Perform logout logic here
+    onLogout();
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Home</Text>
+      <TouchableOpacity style={styles.button} onPress={handleLogout}>
+        <Text style={styles.buttonText}>Logout</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check if there is a token in AsyncStorage
+    AsyncStorage.getItem('token')
+      .then((token) => {
+        if (token) {
+          setLoggedIn(true);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
+
+  return (
+    <View style={styles.container}>
+      {loggedIn ? (
+        <HomePage onLogout={handleLogout} />
+      ) : (
+        <LoginPage onLogin={handleLogin} />
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  error: {
+    color: 'red',
+    marginTop: 10,
+  },
+});
+
+export default App;
