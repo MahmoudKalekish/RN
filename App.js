1cc8a726c521eda5915cdb6705f5653392ece33f
Rana Ghraizi <ranaghraizi1@gmail.com>
2023-03-14 09:26:23 +0200

    started with the login

--- App.js
+++ App.js
@@ -1,87 +1,106 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Dashboard } from './scenes/dashboard/index';
-import { useNavigation } from '@react-navigation/native';
+// import React, { useState } from 'react';
+// import { View, Text, TextInput, Button } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { Dashboard } from './scenes/dashboard/index';
+// import { useNavigation } from '@react-navigation/native';
 
-const Stack = createNativeStackNavigator();
+// const Stack = createNativeStackNavigator();
 
-// async function loginUser(credentials) {
-//   return fetch('http://localhost:8000/api/login', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(credentials)
-//   })
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error('Network response was not ok');
+// // async function loginUser(credentials) {
+// //   return fetch('http://localhost:8000/api/login', {
+// //     method: 'POST',
+// //     headers: {
+// //       'Content-Type': 'application/json'
+// //     },
+// //     body: JSON.stringify(credentials)
+// //   })
+// //   .then(response => {
+// //     if (!response.ok) {
+// //       throw new Error('Network response was not ok');
+// //     }
+// //     return response.json();
+// //   })
+// //   .then(data => {
+// //     // In React Native, use AsyncStorage to save data locally
+// //     // See https://reactnative.dev/docs/asyncstorage
+// //     return data;
+// //   })
+// //   .catch(error => {
+// //     console.error('Error:', error);
+// //     throw error;
+// //   });
+// // }
+
+// // const navigation = useNavigation();
+
+
+
+
+
+// export default function App() {
+//   const [username, setUserName] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [showPassword, setShowPassword] = useState(false);
+
+//   const handleSubmit = async () => {
+//     try {
+//       const response = await loginUser({
+//         username,
+//         password
+//       });
+//       // Save token and is_super value in app state or AsyncStorage
+//       console.log(response.token, response.is_super);
+//       NavigationContainer.navigate('Dashboard'); // Navigate to Dashboard after successful login
+//     } catch (error) {
+//       console.error('Error:', error);
+//       alert('Error: Invalid Username or Password');
 //     }
-//     return response.json();
-//   })
-//   .then(data => {
-//     // In React Native, use AsyncStorage to save data locally
-//     // See https://reactnative.dev/docs/asyncstorage
-//     return data;
-//   })
-//   .catch(error => {
-//     console.error('Error:', error);
-//     throw error;
-//   });
+//   }
+
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Login">
+//           {() => (
+//             <View>
+//               <Text>Username</Text>
+//               <TextInput
+//                 value={username}
+//                 onChangeText={setUserName}
+//               />
+//               <Text>Password</Text>
+//               <TextInput
+//                 value={password}
+//                 onChangeText={setPassword}
+//                 secureTextEntry={!showPassword}
+//               />
+//               <Button title="Submit" onPress={handleSubmit} />
+//             </View>
+//           )}
+//         </Stack.Screen>
+//         {/* Define Dashboard component */}
+//         <Stack.Screen name="Dashboard" component={Dashboard} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
 // }
 
-// const navigation = useNavigation();
 
+import React from 'react'
+import { View, Text } from 'react-native'
+import Login from './scenes/login/Login.js'
 
-
-
-
-export default function App() {
-  const [username, setUserName] = useState('');
-  const [password, setPassword] = useState('');
-  const [showPassword, setShowPassword] = useState(false);
-
-  const handleSubmit = async () => {
-    try {
-      const response = await loginUser({
-        username,
-        password
-      });
-      // Save token and is_super value in app state or AsyncStorage
-      console.log(response.token, response.is_super);
-      NavigationContainer.navigate('Dashboard'); // Navigate to Dashboard after successful login
-    } catch (error) {
-      console.error('Error:', error);
-      alert('Error: Invalid Username or Password');
-    }
-  }
-
+const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login">
-          {() => (
-            <View>
-              <Text>Username</Text>
-              <TextInput
-                value={username}
-                onChangeText={setUserName}
-              />
-              <Text>Password</Text>
-              <TextInput
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry={!showPassword}
-              />
-              <Button title="Submit" onPress={handleSubmit} />
-            </View>
-          )}
-        </Stack.Screen>
-        {/* Define Dashboard component */}
-        <Stack.Screen name="Dashboard" component={Dashboard} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    <View>
+      <Text>
+      hellloooooooooooooo
+      <Login/>
+
+      </Text>
+    </View>
+  )
 }
+
+export default App
--- scenes/login/Login.js
+++ scenes/login/Login.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
+import { Formik } from "formik";
 import {
   StyleSheet,
   Text,
@@ -9,73 +10,183 @@ import {
   Button,
   TouchableOpacity,
 } from "react-native";
+
 export default function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const handleLogin = (values) => {
+    console.log(values);
+  };
+
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Email."
-          placeholderTextColor="#003f5c"
-          onChangeText={(email) => setEmail(email)}
-        /> 
-      </View> 
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Password."
-          placeholderTextColor="#003f5c"
-          secureTextEntry={true}
-          onChangeText={(password) => setPassword(password)}
-        /> 
-      </View> 
-      <TouchableOpacity>
-        <Text style={styles.forgot_button}>Forgot Password?</Text> 
-      </TouchableOpacity> 
-      <TouchableOpacity style={styles.loginBtn}>
-        <Text style={styles.loginText}>LOGIN</Text> 
-      </TouchableOpacity> 
-    </View> 
+      <View style={styles.screen}>
+        <View style={styles.screen__content}>
+          <Formik initialValues={{ email: "", password: "" }} onSubmit={handleLogin}>
+            {({ handleChange, handleSubmit, values }) => (
+              <>
+                <StatusBar style="auto" />
+                <View style={styles.login__field}>
+                  <TextInput
+                    style={styles.login__input}
+                    placeholder="Email"
+                    placeholderTextColor="#003f5c"
+                    onChangeText={handleChange("email")}
+                    value={values.email}
+                  />
+                </View>
+                <View style={styles.login__field}>
+                  <TextInput
+                    style={styles.login__input}
+                    placeholder="Password"
+                    placeholderTextColor="#003f5c"
+                    secureTextEntry={true}
+                    onChangeText={handleChange("password")}
+                    value={values.password}
+                  />
+                </View>
+                <TouchableOpacity style={styles.buttonLogin__submit} onPress={handleSubmit}>
+                  <Text style={styles.button__text}>LOGIN</Text>
+                </TouchableOpacity>
+              </>
+            )}
+          </Formik>
+        </View>
+        <View style={styles.screen__background}>
+          <View style={styles.screen__background__shape4}></View>
+          <View style={styles.screen__background__shape3}></View>
+          <View style={styles.screen__background__shape2}></View>
+          <View style={styles.screen__background__shape1}></View>
+        </View>
+      </View>
+    </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
+    // minHeight: '100vh',
+    overflowY: 'hidden',
+    background: '#141b2d',
   },
-  image: {
-    marginBottom: 40,
+  screen: {		
+    background: 'linear-gradient(90deg, #201c39, #000000)',	
+    position: 'relative',
+    // height: '600px',
+    // width: '500px',
+    boxShadow: '0px 0px 10px #4cceac'
   },
-  inputView: {
-    backgroundColor: "#FFC0CB",
-    borderRadius: 30,
-    width: "70%",
-    height: 45,
-    marginBottom: 20,
-    alignItems: "center",
+  screen__content: {
+    zIndex: 1,
+    position: 'relative',	
+    // height: '100%',
   },
-  TextInput: {
-    height: 50,
+  screen__background: {		
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 0,
     flex: 1,
-    padding: 10,
-    marginLeft: 20,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    // width: 200,
+    // height: 200,
+    backgroundColor: '#f00',
+    borderRadius: 0,
+    overflow: 'hidden',
   },
-  forgot_button: {
-    height: 30,
-    marginBottom: 30,
+  
+  screen__background__shape1: {
+    // height: '520px',
+    // width: '520px',
+    background: '#FFF',
+    // top: '-50px',
+    // right: '120px',
+    // borderRadius: '0 72px 0 0',
+    transform: 'rotate(45deg)',
+    position: 'absolute',
   },
-  loginBtn: {
-    width: "80%",
-    borderRadius: 25,
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    backgroundColor: "#FF1493",
+  
+  screen__background__shape2: {
+    // height: '220px',
+    // width: '220px',
+    background: 'linear-gradient(270deg, #4cceac, #5D54A4 )',
+    // top: '-172px',
+    // right: 0,
+    // borderRadius: '32px',
+    transform: 'rotate(45deg)',
+    position: 'absolute',
+  },
+  
+  screen__background__shape3: {
+    // height: '540px',
+    // width: '190px',
+    background: 'linear-gradient(270deg, #5D54A4, #4cceac)',
+    // top: '-24px',
+    // right: 0,
+    // borderRadius: '32px',
+    transform: 'rotate(45deg)',
+    position: 'absolute',
+  },
+  
+  screen__background__shape4: {
+    // height: '400px',
+    // width: '200px',
+    background: 'linear-gradient(270deg, #4cceac, #5D54A4)',
+    // top: '420px',
+    // right: '50px',
+    // borderRadius: '60px',
+    transform: 'rotate(45deg)',
+    position: 'absolute',
+  },
+  
+  login: {
+    // width: '500px',
+    padding: '30px',
+    paddingTop: '156px',
+  },
+  
+  login__field: {
+    position: 'relative',
+  },
+  
+  login__icon: {
+    position: 'absolute',
+    top: '30px',
+    color: '#7875B5',
+  },
+  
+  login__input: {
+    border: 'none',
+    background: 'none',
+    padding: '10px',
+    paddingLeft: '24px',
+    fontWeight: '700',
+    // width: '75%',
+    transition: '.2s',
+  },
+  
+  login__submit: {
+    background: '#fff',
+    fontSize: '14px',
+    marginTop: '30px',
+    marginLeft: '25px',
+    padding: '16px 20px',
+    borderRadius: '5px',
+    border: '1px solid #D4D3E8',
+    textTransform: 'uppercase',
+    fontWeight: '700',
+    flex: 1,
+    alignItems: 'center',
+    color: '#4C489D',
+    cursor: 'pointer',
+    transition: '.2s',
   },
 });
